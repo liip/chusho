6fe755c2dfd59c146b0f0aadde6b56394ca2b86f
Benoît Burgener <benoit.burgener@gmail.com>
2020-02-22 10:30:39 +0100

    docs: basic Storybook theme

---
+++ packages/chusho/config/storybook/theme.js
@@ -0,0 +1,17 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  appBg: '#EBEFEF',
+  appBorderColor: '#F0DBE4',
+  colorPrimary: '#415350',
+  colorSecondary: '#BF4E81',
+  appBorderRadius: 10,
+
+  textColor: '#2B3634',
+  barTextColor: '#CD749C',
+  barBg: '#FBF4F7',
+
+  brandTitle: 'Chūshō',
+});
