d0fa5f4a0b01c7698105a659d7e411e7d09c4d0c
Benoît Burgener <benoit.burgener@gmail.com>
2020-03-22 09:53:31 +0100

    style: disable expected offense

--- packages/chusho/src/utils/objects.ts
+++ packages/chusho/src/utils/objects.ts
@@ -1,3 +1,4 @@
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 export function isPlainObject(value: any): value is Dictionary<any> {
   return Object.prototype.toString.call(value) === '[object Object]';
 }
