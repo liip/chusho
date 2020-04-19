dbf47efd929a6fadecb25cbb888d6016fa2e494b
Benoît Burgener <benoit.burgener@gmail.com>
2020-04-19 17:55:17 +0200

    test: remove Storyshots
    Components using the composition API are wrongly rendered and others are already covered with other tests

--- packages/chusho/jest.config.js
+++ packages/chusho/jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  transform: {
-    '^.+\\.stories\\.js$': '@storybook/addon-storyshots/injectFileName',
-  },
   testMatch: ['<rootDir>src/**/?(*.)+(spec).[jt]s?(x)'],
   moduleFileExtensions: ['ts', 'js', 'd.ts', 'json', 'jsx', 'tsx', 'node'],
   setupFiles: ['<rootDir>config/jest/setup.js'],
