aad8d9e719108189c87d487325fda215b5faad21
Benoît Burgener <benoit.burgener@gmail.com>
2020-04-26 17:25:11 +0200

    test(Toggle): add e2e tests

--- packages/chusho/src/components/CToggle/CToggle.stories.js
+++ packages/chusho/src/components/CToggle/CToggle.stories.js
@@ -11,8 +11,8 @@ export default {
 export const Default = () => ({
   components: { CToggle, CToggleBtn, CToggleContent },
   template: `<CToggle>
-    <CToggleBtn variant="medium default">Toggle</CToggleBtn>
-    <CToggleContent class="bg-gray-200 px-4 py-3 rounded mt-2">
+    <CToggleBtn variant="medium default" data-test="toggle-button">Toggle</CToggleBtn>
+    <CToggleContent class="bg-gray-200 px-4 py-3 rounded mt-2" data-test="toggle-content">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in, iste id nobis dolor excepturi dolore expedita vero quae. Nobis fuga cupiditate suscipit blanditiis, aliquid minima harum molestias pariatur tempora ab, libero quo maiores sapiente doloribus nihil commodi eaque accusantium praesentium! Nobis natus qui voluptate inventore molestias quisquam, consequuntur harum?
     </CToggleContent>
   </CToggle>`,
@@ -26,8 +26,8 @@ export const Controlled = () => ({
     };
   },
   template: `<CToggle v-model="toggleOpen">
-    <CToggleBtn variant="medium default">{{ toggleOpen ? 'Close' : 'Open' }}</CToggleBtn>
-    <CToggleContent class="bg-gray-200 px-4 py-3 rounded mt-2">
+    <CToggleBtn variant="medium default" data-test="toggle-button">{{ toggleOpen ? 'Close' : 'Open' }}</CToggleBtn>
+    <CToggleContent class="bg-gray-200 px-4 py-3 rounded mt-2" data-test="toggle-content">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in, iste id nobis dolor excepturi dolore expedita vero quae. Nobis fuga cupiditate suscipit blanditiis, aliquid minima harum molestias pariatur tempora ab, libero quo maiores sapiente doloribus nihil commodi eaque accusantium praesentium! Nobis natus qui voluptate inventore molestias quisquam, consequuntur harum?
     </CToggleContent>
   </CToggle>`,
