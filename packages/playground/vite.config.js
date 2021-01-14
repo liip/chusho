f1574effe6c9c56d2d54f4a38edb57590074eb10
Benoît Burgener <benoit.burgener@gmail.com>
2021-01-14 16:43:40 +0100

    chore: clean-up unused dependencies from Vite config

--- packages/playground/vite.config.js
+++ packages/playground/vite.config.js
@@ -1,11 +1,5 @@
 export default {
   optimizeDeps: {
     exclude: ['chusho'],
-    include: [
-      'highlight.js/lib/core',
-      'highlight.js/lib/languages/xml',
-      'prettier/esm/standalone',
-      'prettier/esm/parser-html',
-    ],
   },
 };
