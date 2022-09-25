7c43080d6a4ce953f28bb70e48fe559580ede529
Benoît Burgener <benoit.burgener@gmail.com>
2022-09-25 17:55:38 +0200

    chore: allow to run Vitest from any directory

--- packages/chusho/vitest.config.js
+++ packages/chusho/vitest.config.js
@@ -1,10 +1,11 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['./test/setup.js'],
+    setupFiles: [resolve(__dirname, 'test/setup.js')],
     coverage: {
       reporter: ['html', 'lcov', 'text'],
       include: ['lib/**/*.ts'],
